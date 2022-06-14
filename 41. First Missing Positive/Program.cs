a45e56feb60241c1af78ceb98f1b084d986a00bc
Yiseul Ko <gls20020128@gmail.com>
2022-06-13 21:04:58 -0400

    41. First Missing Positive

---
+++ 41. First Missing Positive/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _41.First_Missing_Positive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+
+            int[] nums = { 7, 8, 9, 11, 12 };
+            Console.WriteLine(s.FirstMissingPositive(nums)); // 1
+
+            int[] nums2 = { 3, 4, -1, 1 };
+            Console.WriteLine(s.FirstMissingPositive(nums2)); // 2
+        }
+    }
+
+    public class Solution
+    {
+        public int FirstMissingPositive(int[] nums)
+        {
+            bool[] found = new bool[nums.Length + 2]; //too much memory?
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (0 < nums[i] && nums[i] < nums.Length + 2)
+                {
+                    found[nums[i]] = true;
+                }
+            }
+
+            //         Console.WriteLine("[");
+            //         for(int i = 1; i<found.Length; i++){
+            //              Console.WriteLine(found[i] + ", ");
+            //         }
+            //         Console.WriteLine("]");
+
+            int missing = -1;
+            for (int i = 1; i < found.Length; i++)
+            {
+                if (!found[i])
+                {
+                    missing = i;
+                    break;
+                }
+            }
+
+            return missing;
+        }
+    }
+}
