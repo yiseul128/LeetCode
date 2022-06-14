b2ac8a786d65eef0493214e1b14476760849cec8
Yiseul Ko <gls20020128@gmail.com>
2022-06-13 21:27:45 -0400

    383. Note

---
+++ 383. Note/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _383.Note
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+
+            string note = "aa", magazine = "aab";
+            Console.WriteLine(s.CanConstruct(note, magazine)); //true
+
+            magazine = "ab";
+            Console.WriteLine(s.CanConstruct(note, magazine)); //false
+        }
+    }
+
+    public class Solution
+    {
+        public bool CanConstruct(string note, string magazine)
+        {
+            bool found = false;
+            for (int i = 0; i < note.Length; i++)
+            {
+                found = false;
+                for (int j = 0; j < magazine.Length; j++)
+                {
+                    if (note[i] == magazine[j])
+                    {
+                        found = true;
+                        magazine = magazine.Remove(j, 1);
+                        //Console.WriteLine(magazine);
+                        break;
+                    }
+                }
+                if (found == false)
+                {
+                    return false;
+                }
+            }
+            if (found)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
