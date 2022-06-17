dc628e991b6fb2199ea5278e98e191511f9c736f
Yiseul Ko <gls20020128@gmail.com>
2022-06-17 11:39:26 -0400

    162. Find Peak Element and 14. Longest Common Prefix

---
+++ 14. Longest Common Prefix/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14.Longest_Common_Prefix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+            string[] strs = {"flower", "flow", "flight"};
+            Console.WriteLine(s.LongestCommonPrefix(strs)); //fl
+        }
+    }
+
+    public class Solution
+    {
+        public string LongestCommonPrefix(string[] strs)
+        {
+
+            if (strs.Length == 1)
+            {
+                return strs[0];
+            }
+
+            int minLength = strs[0].Length;
+
+            //get min length of strs elements
+            for (int i = 0; i < strs.Length - 1; i++)
+            {
+                if (strs[i].Length > strs[i + 1].Length)
+                {
+                    minLength = minLength > strs[i + 1].Length ? strs[i + 1].Length : minLength;
+                }
+                else
+                {
+                    minLength = minLength > strs[i].Length ? strs[i].Length : minLength;
+                }
+
+                //Console.WriteLine(minLength);
+            }
+
+            string prefix = "";
+            for (int i = 0; i < minLength; i++)
+            {
+
+                for (int j = 1; j < strs.Length; j++)
+                {
+                    if (strs[j][i] != strs[j - 1][i])
+                    {
+                        return prefix;
+                    }
+                }
+
+                prefix += strs[0][i].ToString();
+                //Console.WriteLine(prefix);
+            }
+
+            return prefix;
+        }
+    }
+}
---
+++ 162. Find Peak Element/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _162.Find_Peak_Element
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+            int[] nums = { 1, 2, 1, 3, 5, 6, 4 };
+            Console.WriteLine(s.FindPeakElement(nums)); //1
+
+        }
+    }
+
+    public class Solution
+    {
+        public int FindPeakElement(int[] nums)
+        {
+            //only one element
+            if (nums.Length == 1)
+            {
+                return 0;
+            }
+
+            //first or last is peak
+            if (nums[0] > nums[1])
+            {
+                return 0;
+            }
+            if (nums[nums.Length - 1] > nums[nums.Length - 2])
+            {
+                return nums.Length - 1;
+            }
+
+            //regular
+            for (int i = 1; i < nums.Length - 1; i++)
+            {
+                if (nums[i] > nums[i - 1] && nums[i] > nums[i + 1])
+                {
+                    return i;
+                }
+            }
+
+            return 0;
+        }
+    }
+}
