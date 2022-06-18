47b5f600ce0956da5d4f2db340dbdfb673729af2
Yiseul Ko <gls20020128@gmail.com>
2022-06-18 19:27:10 -0400

    283. Move Zeroes

---
+++ 283. Move Zeroes/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _283.Move_Zeroes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    public class Solution
+    {
+        public void MoveZeroes(int[] nums)
+        {
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == 0)
+                {
+                    int j = i + 1;
+                    for (; j < nums.Length; j++)
+                    {
+                        if (nums[j] != 0)
+                        {
+                            int temp = nums[j];
+                            nums[j] = nums[i];
+                            nums[i] = temp;
+                            break;
+                        }
+                    }
+                    if (j == nums.Length - 1)
+                    {
+                        return; //non zero not found;
+                    }
+
+                }
+            }
+        }
+    }
+}
