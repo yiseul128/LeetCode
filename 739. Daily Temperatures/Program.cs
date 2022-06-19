fca8bc2bb46d87af59f8e1aaf70be432c64e2912
Yiseul Ko <gls20020128@gmail.com>
2022-06-19 15:55:18 -0400

    739. Daily Temperatures

---
+++ 739. Daily Temperatures/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _739.Daily_Temperatures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class Solution
+    {
+        public int[] DailyTemperatures(int[] temperatures)
+        {
+            int[] answer = new int[temperatures.Length];
+
+            for (int i = 0; i < temperatures.Length; i++)
+            {
+                for (int j = i + 1; j < temperatures.Length; j++)
+                {
+                    if (temperatures[i] < temperatures[j])
+                    {
+                        answer[i] = j - i;
+                        break;
+                    }
+                }
+            }
+
+            return answer;
+        }
+    }
+}
