3e33c1d38be822ff712050835907d004a0ee2a71
Yiseul Ko <gls20020128@gmail.com>
2022-06-21 18:11:30 -0400

    1. Two Sum

---
+++ 1. Two Sum/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.Two_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    public class Solution
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            int[] answer = new int[2];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        answer[0] = i;
+                        answer[1] = j;
+                        return answer;
+                    }
+                }
+            }
+
+            return answer;
+
+        }
+    }
+}
