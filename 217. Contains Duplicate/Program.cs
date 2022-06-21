96c966e5b6b6c39e2efc0648a5bf8f2d7579ab24
Yiseul Ko <gls20020128@gmail.com>
2022-06-21 18:15:07 -0400

    217. Contains Duplicate

---
+++ 217. Contains Duplicate/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _217.Contains_Duplicate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+            int[] nums = { 1, 2, 3, 4 }; //false
+            Console.WriteLine(s.ContainsDuplicate(nums));
+        }
+    }
+    public class Solution
+    {
+        public bool ContainsDuplicate(int[] nums)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] == nums[j])
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+    }
+}
