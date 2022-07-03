6291bee78a2956d81f425b7e2fefe30483a99c7c
Yiseul Ko <gls20020128@gmail.com>
2022-07-03 15:46:13 -0400

    242. Valid Anagram

---
+++ 242. Valid Anagram/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _242.Valid_Anagram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+            Console.WriteLine(s.IsAnagram("anagram", "nagaram")); //true
+        }
+    }
+    public class Solution
+    {
+        public bool IsAnagram(string s, string t)
+        {
+
+            if (s.Length != t.Length)
+            {
+                return false;
+            }
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                for (int j = 0; j < t.Length; j++)
+                {
+                    if (s[i] == t[j])
+                    {
+
+                        t = t.Substring(0, j) + t.Substring(j + 1);
+                        //Console.WriteLine(t);
+                        break;
+                    }
+
+                    if (j == t.Length - 1)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            if (t.Length > 0)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
