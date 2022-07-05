bd1e728f47ed8063806e460567ade9af5d8808fd
Yiseul Ko <gls20020128@gmail.com>
2022-07-04 22:18:13 -0400

    49. Group Anagrams

---
+++ 49. Group Anagrams/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _49.Group_Anagrams
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    public class Solution
+    {
+        public IList<IList<string>> GroupAnagrams(string[] strs)
+        {
+
+            IList<IList<string>> ans = new List<IList<string>>();
+
+            string[] sorted = new string[strs.Length];
+            for (int i = 0; i < strs.Length; i++)
+            {
+                sorted[i] = new string(strs[i].OrderBy(c => c).ToArray());
+                //Console.WriteLine(sorted[i]);
+            }
+
+            bool[] taken = new bool[strs.Length];
+            for (int i = 0; i < strs.Length; i++)
+            {
+                if (taken[i] == true)
+                {
+                    continue;
+                }
+
+                IList<string> forThis = new List<string>();
+                forThis.Add(strs[i]);
+                taken[i] = true;
+                for (int j = i + 1; j < strs.Length; j++)
+                {
+                    if (sorted[j] == sorted[i])
+                    {
+                        forThis.Add(strs[j]);
+                        taken[j] = true;
+                    }
+                }
+
+                ans.Add(forThis);
+            }
+
+            return ans;
+        }
+    }
+}
