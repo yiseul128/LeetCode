1200949eeeb54dd6d130aa77c873a768ac6cc6c9
Yiseul Ko <gls20020128@gmail.com>
2022-07-05 18:27:38 -0400

    125. Valid Palindrome

---
+++ 125. Valid Palindrome/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _125.Valid_Palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+            Console.WriteLine(s.IsPalindrome("A man, a plan, a canal: Panama")); //true
+        }
+    }
+    public class Solution
+    {
+        public bool IsPalindrome(string s)
+        {
+
+            s = new String(s.Where(Char.IsLetterOrDigit).ToArray());
+            s = s.ToLower();
+            //Console.WriteLine(s);
+
+            for (int i = 0; i < s.Length / 2; i++)
+            {
+                if (s[i] != s[s.Length - 1 - i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
