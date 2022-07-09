395e7dfbcfc2a02a026820e14cfe83ae281054a1
Yiseul Ko <gls20020128@gmail.com>
2022-07-09 16:46:42 -0400

    206. Reverse Linked List

---
+++ 206. Reverse Linked List/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _206.Reverse_Linked_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+    
+    public class Solution
+    {
+        public ListNode ReverseList(ListNode head)
+        {
+
+            if (head == null || head.next == null)
+            {
+                return head;
+            }
+
+            ListNode prevOne = head;
+            ListNode currOne = head.next;
+            ListNode nextOne;
+            prevOne.next = null; //handle head
+
+            while (currOne.next != null)
+            {
+                nextOne = currOne.next;
+                currOne.next = prevOne;
+
+                prevOne = currOne;
+                currOne = nextOne;
+            }
+
+            currOne.next = prevOne; //handle tail
+
+            return currOne;
+
+        }
+    }
+}
