46d7b184056894923168a688c78687f7b2080a0b
Yiseul Ko <gls20020128@gmail.com>
2022-07-10 13:54:53 -0400

    21. Merge Two Sorted Lists

---
+++ 21. Merge Two Sorted Lists/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21.Merge_Two_Sorted_Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    /* Definition for singly-linked list. */
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+    public class Solution
+    {
+        public ListNode MergeTwoLists(ListNode list1, ListNode list2)
+        {
+
+            if (list1 == null)
+            {
+                return list2;
+            }
+            if (list2 == null)
+            {
+                return list1;
+            }
+
+            //handle head
+            ListNode head;
+            if (list1.val < list2.val)
+            {
+                head = list1;
+                list1 = list1.next;
+            }
+            else
+            {
+                head = list2;
+                list2 = list2.next;
+            }
+
+            ListNode curNode = head;
+            while (list1 != null && list2 != null)
+            {
+                if (list1.val < list2.val)
+                {
+                    curNode.next = list1;
+                    list1 = list1.next;
+
+                }
+                else
+                {
+                    curNode.next = list2;
+                    list2 = list2.next;
+                }
+                curNode = curNode.next;
+            }
+
+            if (list1 != null)
+            {
+                curNode.next = list1;
+            }
+            if (list2 != null)
+            {
+                curNode.next = list2;
+            }
+
+            return head;
+
+        }
+    }
+}
