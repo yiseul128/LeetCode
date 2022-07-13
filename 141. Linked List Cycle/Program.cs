60de7cedf92e040d808e55748db1d6108df29710
Yiseul Ko <gls20020128@gmail.com>
2022-07-12 20:31:55 -0400

    141. Linked List Cycle

---
+++ 141. Linked List Cycle/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _141.Linked_List_Cycle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    /* Definition for singly-linked list. */
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int x)
+        {
+            val = x;
+            next = null;
+        }
+    }
+
+    public class Solution
+    {
+        public bool HasCycle(ListNode head)
+        {
+            ListNode n = new ListNode(0);
+
+            ListNode curNode = null;
+            while (head != null)
+            {
+                curNode = head;
+                head = head.next;
+                if (curNode.next == n)
+                {
+                    return true;
+                }
+                curNode.next = n; //set it
+            }
+            return false;
+        }
+    }
+}
