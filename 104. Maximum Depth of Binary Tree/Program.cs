e2b3527d0352fea3dba2e8ba8779caea148558e8
Yiseul Ko <gls20020128@gmail.com>
2022-07-19 15:04:40 -0400

    104. Maximum Depth of Binary Tree

---
+++ 104. Maximum Depth of Binary Tree/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _104.Maximum_Depth_of_Binary_Tree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    /* Definition for a binary tree node. */
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+ 
+    public class Solution
+    {
+        public int MaxDepth(TreeNode root)
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+
+            return RecurMaxDepth(root);
+
+        }
+
+        public int RecurMaxDepth(TreeNode node)
+        {
+            if (node.left == null && node.right == null)
+            {
+                return 1;
+            }
+
+            int right = 0;
+            int left = 0;
+            if (node.left != null)
+            {
+                left += RecurMaxDepth(node.left);
+            }
+            if (node.right != null)
+            {
+                right += RecurMaxDepth(node.right);
+            }
+
+            int depth = left > right ? left : right;
+            depth++;
+            return depth;
+        }
+    }
+}
