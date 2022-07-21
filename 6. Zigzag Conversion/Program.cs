96bd924e9bcc334e3152228a632244b4a7c44ba8
Yiseul Ko <gls20020128@gmail.com>
2022-07-20 20:18:16 -0400

    6. Zigzag Conversion

---
+++ 6. Zigzag Conversion/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6.Zigzag_Conversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    public class Solution
+    {
+        public string Convert(string s, int numRows)
+        {
+            if (numRows == 1)
+            {
+                return s;
+            }
+
+            Queue<char>[] rows = new Queue<char>[numRows];
+
+            for (int i = 0; i < numRows; i++)
+            {
+                rows[i] = new Queue<char>();
+            }
+
+            int r = 0;
+            bool positive = true;
+            for (int i = 0; i < s.Length; i++)
+            {
+
+                rows[r].Enqueue(s[i]);
+
+                if (r == 0)
+                {
+                    positive = true;
+                }
+                else if (r == numRows - 1)
+                {
+                    positive = false;
+                }
+
+                if (positive)
+                {
+                    r++;
+                }
+                else
+                {
+                    r--;
+                }
+            }
+
+            string answer = "";
+            for (int i = 0; i < numRows; i++)
+            {
+                while (rows[i].Count != 0)
+                {
+                    answer += rows[i].Dequeue().ToString();
+                }
+            }
+
+            return answer;
+        }
+    }
+}
