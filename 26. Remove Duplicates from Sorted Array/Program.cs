0660e0fc1005b74654df07606c575c88721a2dfd
Yiseul Ko <gls20020128@gmail.com>
2022-07-27 17:37:55 -0400

    26. Remove Duplicates from Sorted Array

---
+++ 26. Remove Duplicates from Sorted Array/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _26.Remove_Duplicates_from_Sorted_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    public class Solution
+    {
+        public int RemoveDuplicates(int[] nums)
+        {
+            int count = 1;
+            int prev = nums[0];
+
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (prev != nums[i])
+                {
+                    nums[count] = nums[i];
+                    count++;
+                    prev = nums[i];
+                }
+            }
+
+            return count;
+        }
+    }
+}
