76101980fdea1b2c8e59c76ebb5e01d36e00b97d
Yiseul Ko <gls20020128@gmail.com>
2022-08-06 00:22:10 -0400

    202. Happy Number

---
+++ 202. Happy Number/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _202.Happy_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    public class Solution
+    {
+        HashSet<int> nums = new HashSet<int>();
+
+        public bool IsHappy(int n)
+        {
+            int l = n.ToString().Length;
+            int result = 0;
+            for (int i = l - 1; i >= 0; i--)
+            {
+                int digit = n / Convert.ToInt32(Math.Pow(10, i));
+                Console.WriteLine(digit);
+                result += digit * digit;
+                n %= Convert.ToInt32(Math.Pow(10, i));
+
+            }
+
+            if (nums.Contains(result))
+            {
+                return false;
+            }
+
+            if (result == 1)
+            {
+                return true;
+            }
+
+            nums.Add(result);
+            return IsHappy(result);
+
+        }
+    }
+}
