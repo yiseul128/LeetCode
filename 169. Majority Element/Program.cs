25ef8b55ace044fdb04bb03adce7a70533d3c394
Yiseul Ko <gls20020128@gmail.com>
2022-08-07 20:48:29 -0400

    169. Majority Element

---
+++ 169. Majority Element/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _169.Majority_Element
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    public class Solution
+    {
+        public int MajorityElement(int[] nums)
+        {
+            Dictionary<int, int> freq = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (freq.ContainsKey(nums[i]))
+                {
+                    freq[nums[i]]++;
+                }
+                else
+                {
+                    freq[nums[i]] = 1;
+                }
+
+                if (freq[nums[i]] > nums.Length / 2)
+                {
+                    return nums[i];
+                }
+            }
+
+            return 0;
+        }
+    }
+}
