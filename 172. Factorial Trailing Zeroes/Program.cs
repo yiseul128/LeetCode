00081226b1955f1b1fa21268bc7f172666fed947
Yiseul Ko <gls20020128@gmail.com>
2022-08-19 01:08:14 -0400

    172. Factorial Trailing Zeroes

---
+++ 172. Factorial Trailing Zeroes/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _172.Factorial_Trailing_Zeroes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    public class Solution
+    {
+        public int TrailingZeroes(int n)
+        {
+            //number of 0 = number of 10 = number of 5 when factored
+
+            int fives = 0;
+
+            int multiplied = 5;
+            while (multiplied <= n)
+            {
+                int c = 0;
+                int cur = multiplied;
+
+                while (true)
+                {
+                    if (cur % 5 == 0)
+                    {
+                        cur /= 5;
+                        c++;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                fives += c;
+                //Console.WriteLine(fives);
+                multiplied += 5;
+            }
+
+            return fives;
+        }
+    }
+}
