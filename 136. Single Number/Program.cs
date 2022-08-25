99c9b956441faf0e6eaee3e93297fcfc3f4e38ce
Yiseul Ko <gls20020128@gmail.com>
2022-08-25 13:14:47 -0400

    136. Single Number

---
+++ 136. Single Number/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _136.Single_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    public class Solution
+    {
+        public int SingleNumber(int[] nums)
+        {
+            HashSet<int> numbers = new HashSet<int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (numbers.Contains(nums[i]))
+                {
+                    numbers.Remove(nums[i]);
+                }
+                else
+                {
+                    numbers.Add(nums[i]);
+                }
+            }
+
+            foreach (int i in numbers)
+            {
+                return i;
+            }
+
+            return -1;
+        }
+    }
+}
