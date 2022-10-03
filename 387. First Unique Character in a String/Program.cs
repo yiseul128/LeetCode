fca7518f3b6a4c73d7dd5e7a2b28c6ab8f094b3a
Yiseul Ko <gls20020128@gmail.com>
2022-10-02 22:23:50 -0400

    387. First Unique Character in a String

---
+++ 387. First Unique Character in a String/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Specialized;
+using System.Collections;
+
+namespace _387.First_Unique_Character_in_a_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+
+    public class Solution
+    {
+        public int FirstUniqChar(string s)
+        {
+            OrderedDictionary chars = new OrderedDictionary();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                string strC = s[i].ToString();
+                if (chars.Contains(strC))
+                {
+                    // Console.WriteLine(chars[s[i]]);
+                    chars[strC] = true;
+                }
+                else
+                {
+                    chars.Add(strC, false);
+                }
+            }
+
+            ICollection keyCollection = chars.Keys;
+
+            foreach (string c in keyCollection)
+            {
+                if ((bool)chars[c] == false)
+                {
+                    return s.IndexOf(c);
+                }
+            }
+            return -1;
+        }
+    }
+}
