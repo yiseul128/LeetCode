4b00f5f72f532bedb1937c4c1ce7342cdf11310d
Yiseul Ko <gls20020128@gmail.com>
2022-10-23 18:19:07 -0400

    287. Find the Duplicate Number

---
+++ 287. Find the Duplicate Number/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _287.Find_the_Duplicate_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class Solution
+    {
+        public int FindDuplicate(int[] nums)
+        {
+            bool[] found = new bool[nums.Length];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (found[nums[i]])
+                {
+                    return nums[i];
+                }
+                found[nums[i]] = true;
+            }
+
+            return 0;
+        }
+    }
+}
