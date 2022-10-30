47eeeb4e25c352870b71df56818b44327f691334
Yiseul Ko <gls20020128@gmail.com>
2022-10-30 17:30:54 -0400

    412. Fizz Buzz

---
+++ 412. Fizz Buzz/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _412.Fizz_Buzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    public class Solution
+    {
+        public IList<string> FizzBuzz(int n)
+        {
+            IList<string> answer = new List<string>();
+            for (int i = 1; i <= n; i++)
+            {
+                if (i % 5 == 0 && i % 3 == 0)
+                {
+                    answer.Add("FizzBuzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    answer.Add("Fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    answer.Add("Buzz");
+                }
+                else
+                {
+                    answer.Add(i.ToString());
+                }
+            }
+
+            return answer;
+        }
+    }
+}
