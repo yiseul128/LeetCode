c956d9714a2f6d56ff78e6dd9d54730ebd054828
Yiseul Ko <gls20020128@gmail.com>
2022-10-31 21:42:02 -0400

    344. Reverse String

---
+++ 344. Reverse String/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _344.Reverse_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class Solution
+    {
+        public void ReverseString(char[] s)
+        {
+            int len = s.Length - 1;
+            int half = len % 2 == 0 ? len / 2 : len / 2 + 1;
+
+            for (int i = 0; i < half; i++)
+            {
+                char temp = s[i];
+                s[i] = s[len - i];
+                s[len - i] = temp;
+            }
+        }
+    }
+}
