939a034ae7af5b3e7e9c82eb04d32a98ad816d22
Yiseul Ko <gls20020128@gmail.com>
2022-11-10 23:42:37 -0500

    240. Search a 2D Matrix II

---
+++ 240. Search a 2D Matrix II/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _240.Search_a_2D_Matrix_II
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    public class Solution
+    {
+        public bool SearchMatrix(int[][] matrix, int target)
+        {
+
+            bool bigger = true;
+            int c = 0;
+
+            for (int r = 0; r < matrix.Length; r++)
+            {
+                if (c == -1)
+                {
+                    // Console.WriteLine("min");
+                    c++;
+                }
+                if (c == matrix[0].Length)
+                {
+                    // Console.WriteLine("max");
+                    c--;
+                }
+
+                if (matrix[r][c] > target)
+                {
+                    bigger = false;
+                }
+                else if (matrix[r][c] < target)
+                {
+                    bigger = true;
+                }
+
+                while (c >= 0 && c < matrix[0].Length)
+                {
+                    // found target
+                    if (matrix[r][c] == target)
+                    {
+                        return true;
+                    }
+
+                    if (matrix[r][c] > target)
+                    {
+                        if (bigger)
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            c--;
+                        }
+
+                    }
+                    else
+                    {
+                        if (bigger)
+                        {
+                            c++;
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                }
+            }
+
+            return false;
+        }
+    }
+}
