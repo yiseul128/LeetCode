c6d68e2aecb31a87f29cdd24dffea9a9d4d077da
Yiseul Ko <gls20020128@gmail.com>
2022-11-12 11:37:56 -0500

    326. Power of Three

---
+++ 326. Power of Three/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _326.Power_of_Three
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    public class Solution
+    {
+        public bool IsPowerOfThree(int n)
+        {
+            if (n <= 0)
+            {
+                return false;
+            }
+
+            while (n != 1)
+            {
+                if (n % 3 == 0)
+                {
+                    n /= 3;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
