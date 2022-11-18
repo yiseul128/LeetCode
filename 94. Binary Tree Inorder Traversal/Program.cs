c35b9097a8f629548dd9171354a460f3df0cbc2d
Yiseul Ko <gls20020128@gmail.com>
2022-11-17 21:50:33 -0500

    94. Binary Tree Inorder Traversal

---
+++ 94. Binary Tree Inorder Traversal/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _94.Binary_Tree_Inorder_Traversal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    /* Definition for a binary tree node. */
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    public class Solution
+    {
+        public IList<int> InorderTraversal(TreeNode root)
+        {
+            List<int> nodes = new List<int>();
+
+            Tranverse(root, nodes);
+
+            return nodes;
+        }
+
+        public void Tranverse(TreeNode n, List<int> nodes)
+        {
+            if (n == null)
+            {
+                return;
+            }
+
+            Tranverse(n.left, nodes);
+            nodes.Add(n.val);
+            Tranverse(n.right, nodes);
+        }
+    }
+}
