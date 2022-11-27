9a5bd1d6f2dd1f4e919f54ada100573cac2b84f4
Yiseul Ko <gls20020128@gmail.com>
2022-11-27 13:01:18 -0500

    50. Pow(x, n)

---
+++ 50. Pow(x, n)/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _50.Pow_x__n_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    public class Solution
+    {
+        public double MyPow(double x, int n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+            else if (n < 0)
+            {
+                return 1.0 / RecurPow(x, Convert.ToInt64(n) * -1);
+            }
+            else
+            {
+                return RecurPow(x, Convert.ToInt64(n));
+            }
+        }
+
+        public double RecurPow(double x, long n)
+        {
+            // Console.WriteLine(n);
+            if (n == 1)
+            {
+                return x;
+            }
+
+            double half = RecurPow(x * x, n / 2);
+            if (n % 2 == 0)
+            {
+                return half;
+            }
+            else
+            {
+                return half * x;
+            }
+        }
+    }
+}
