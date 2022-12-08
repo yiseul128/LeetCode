44ac437b8326c815bfbd40917ba81f64347793b3
Yiseul Ko <gls20020128@gmail.com>
2022-12-07 20:30:36 -0500

    142. Linked List Cycle II

---
+++ 142. Linked List Cycle II/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _142.Linked_List_Cycle_II
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    /* Definition for singly-linked list. */
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int x)
+        {
+            val = x;
+            next = null;
+        }
+    }
+ 
+    public class Solution
+    {
+        public ListNode DetectCycle(ListNode head)
+        {
+            HashSet<ListNode> set = new HashSet<ListNode>();
+
+            while (head != null)
+            {
+                if (set.Contains(head.next))
+                {
+                    return head.next;
+                }
+                set.Add(head);
+                head = head.next;
+            }
+            return null;
+        }
+    }
+}
