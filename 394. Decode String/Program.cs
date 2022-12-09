1ce91a8b52f671589b8f722a027a720d2c29f76b
Yiseul Ko <gls20020128@gmail.com>
2022-12-08 21:40:19 -0500

    394. Decode String

---
+++ 394. Decode String/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _394.Decode_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    public class Solution
+    {
+        string num = "0123456789";
+        int end = -1;
+
+        public string DecodeString(string s)
+        {
+            return Decode(s, 0);
+        }
+
+        public string Decode(string str, int start)
+        {
+            string decoded = "";
+
+            int numStart = -1;
+            while (start < str.Length)
+            {
+                if (str[start] == '[')
+                {
+                    decoded += String.Concat(Enumerable.Repeat(Decode(str, start + 1), Convert.ToInt32(str.Substring(numStart, start - numStart))));
+                    start = end;
+                    numStart = -1;
+                }
+                else if (str[start] == ']')
+                {
+                    end = start;
+                    return decoded;
+                }
+                else if (num.Contains(str[start]))
+                {
+                    if (numStart == -1)
+                    {
+                        numStart = start;
+                    }
+                }
+                else
+                {
+                    decoded += str[start].ToString();
+                }
+                start++;
+            }
+
+            return decoded;
+        }
+    }
+}
