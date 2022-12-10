a78b4158a4d4ab7c1dc69486e1760de65b05c716
Yiseul Ko <gls20020128@gmail.com>
2022-12-10 00:12:10 -0500

    35. Search Insert Position

---
+++ 35. Search Insert Position/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _35.Search_Insert_Position
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    public class Solution
+    {
+        public int SearchInsert(int[] nums, int target)
+        {
+            int start = 0;
+            int end = nums.Length - 1;
+            int middle = (start + end) / 2;
+
+            if (nums[start] >= target)
+            {
+                return 0;
+            }
+            if (nums[end] < target)
+            {
+                return end + 1;
+            }
+
+            while (start < end)
+            {
+                middle = (start + end) / 2;
+                if (nums[middle] == target)
+                {
+                    return middle;
+                }
+                else if (nums[middle] < target)
+                {
+                    start = middle + 1;
+                }
+                else
+                {
+                    end = middle - 1;
+                }
+            }
+            return nums[end] >= target ? end : end + 1;
+        }
+    }
+}
