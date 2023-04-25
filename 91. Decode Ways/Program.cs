45b848fd9c6352e59e87ae12c0b1b079f6d224ff
Yiseul Ko <gls20020128@gmail.com>
2023-04-24 23:53:55 -0400

    91. Decode Ways

---
+++ 91. Decode Ways/Program.cs
@@ -0,0 +1,46 @@
+﻿public class Solution
+{
+    int[] counts;
+
+    public int NumDecodings(string s)
+    {
+        counts = new int[s.Length];
+        for (int i = 0; i < s.Length; i++)
+        {
+            counts[i] = -1;
+        }
+        return RecurDecode(s, 0);
+    }
+
+    public int RecurDecode(String s, int idx)
+    {
+        // base case
+        if (s.Length == 0)
+        {
+            return 1;
+        }
+        if (s[0] == '0')
+        {
+            counts[idx] = 0;
+            return 0;
+        }
+        if (counts[idx] > 0)
+        {
+            return counts[idx];
+        }
+
+        int n = 0;
+
+        // cut 1 digit
+        n += RecurDecode(s.Substring(1), idx + 1);
+
+        // cut 2 digits
+        if (s.Length > 1 && Int32.Parse(s.Substring(0, 2)) < 27)
+        {
+            n += RecurDecode(s.Substring(2), idx + 2);
+        }
+
+        counts[idx] = n;
+        return n;
+    }
+}
