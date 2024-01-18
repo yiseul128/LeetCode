f2152b965103ff728c125d4be89dab268c390a11
Yiseul Ko <gls20020128@gmail.com>
2024-01-17 19:29:03 -0500

    Recursive solution for 25. Reverse Nodes in k-Group

--- 25. Reverse Nodes in k-Group/Program.cs
+++ 25. Reverse Nodes in k-Group/Program.cs
@@ -10,8 +10,21 @@ namespace _25.Reverse_Nodes_in_k_Group
     {
         static void Main(string[] args)
         {
+            ListNode n1 = new ListNode(1);
+            ListNode n2 = new ListNode(2);
+            ListNode n3 = new ListNode(3);
+            ListNode n4 = new ListNode(4);
+            ListNode n5 = new ListNode(5);
+            n1.next = n2;
+            n2.next = n3;
+            n3.next = n4; 
+            n4.next = n5;
+
+            Solution s = new Solution();
+            ListNode listNode = s.ReverseKGroup(n1, 2);
         }
     }
+
     /* Definition for singly-linked list. */
     public class ListNode
     {
@@ -23,9 +36,42 @@ namespace _25.Reverse_Nodes_in_k_Group
             this.next = next;
         }
     }
- 
+
     public class Solution
     {
+        public ListNode RecursiveReverse(ListNode head, ListNode prevEnd, int k, int count)
+        {
+            // check if possible
+            if (count == 0)
+            {
+                return head;
+            }
+
+            // reverse one unit
+            ListNode prev = null;
+            ListNode curr = head;
+            ListNode tmpNext = null;
+            for (int j = 0; j < k; j++)
+            {
+                tmpNext = curr.next;
+                curr.next = prev;
+                prev = curr;
+                curr = tmpNext;
+            }
+
+            head.next = curr;
+            if (prevEnd != null)
+            {
+                prevEnd.next = prev;
+            }
+
+            // recurr 
+            RecursiveReverse(curr, head, k, --count);
+
+            // return head
+            return prev;
+        }
+
         public ListNode ReverseKGroup(ListNode head, int k)
         {
             // traverse to get count
@@ -38,60 +84,50 @@ namespace _25.Reverse_Nodes_in_k_Group
                 len++;
             }
 
-            int count = len / k;
+            return RecursiveReverse(head, null, k, len / k);
 
-            // loop 
-            bool headFound = false;
-            ListNode newHead = head;
-            ListNode prevEnd = null;
-            ListNode currEnd = null;
-            ListNode prev = null;
-            ListNode curr = head;
-            ListNode tmpNext = null;
-            for (int i = 0; i < count; i++)
-            {
-                for (int j = 0; j < k; j++)
-                {
-                    tmpNext = curr.next;
+            /* iterative */
+            // int count = len/k;
 
-                    // first
-                    if (j == 0)
-                    {
-                        currEnd = curr;
-                    }
-                    else
-                    {
-                        curr.next = prev;
+            // // loop 
+            // bool headFound = false;
+            // ListNode newHead = head;
+            // ListNode prevEnd = null;
+            // ListNode currEnd = null;
+            // ListNode prev = null;
+            // ListNode curr = head;
+            // ListNode tmpNext = null;
+            // for(int i = 0; i < count; i++){
+            //     for(int j = 0; j < k; j++){
+            //         tmpNext = curr.next;
 
-                        // last
-                        if (j == k - 1)
-                        {
-                            if (prevEnd != null)
-                            {
-                                prevEnd.next = curr;
-                            }
-                            prevEnd = currEnd;
+            //         // first
+            //         if(j == 0){
+            //             currEnd = curr;
+            //         }
+            //         else{
+            //             curr.next = prev;
 
-                            if (!headFound)
-                            {
-                                newHead = curr;
-                                headFound = true;
-                            }
-                        }
-                    }
-                    prev = curr;
-                    curr = tmpNext;
-                }
-            }
-            currEnd.next = curr;
+            //             // last
+            //             if(j == k-1){
+            //                 if(prevEnd != null){
+            //                     prevEnd.next = curr;
+            //                 }
+            //                 prevEnd = currEnd;
 
-            // ListNode n = newHead;
-            // while(n!= null){
-            //     Console.WriteLine(n.val);
-            //     n = n.next;
+            //                 if(!headFound){
+            //                     newHead = curr;
+            //                     headFound = true;
+            //                 }
+            //             }
+            //         }
+            //         prev = curr;
+            //         curr = tmpNext;
+            //     }   
             // }
+            // currEnd.next = curr;
 
-            return newHead;
+            // return newHead;
         }
     }
 }
