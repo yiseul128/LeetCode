f1ee61bc35f837a748a9be372fbdff68755a229e
Yiseul Ko <gls20020128@gmail.com>
2024-01-18 18:39:40 -0500

    27. Remove Element

---
+++ 27. Remove Element/Program.cs
@@ -0,0 +1,22 @@
+﻿Solution s = new Solution();
+int[] a = { 0, 1, 2, 1, 2 };
+Console.WriteLine(s.RemoveElement(a, 2));
+
+public class Solution
+{
+    public int RemoveElement(int[] nums, int val)
+    {
+        int count = 0;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] != val)
+            {
+                nums[count] = nums[i];
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
