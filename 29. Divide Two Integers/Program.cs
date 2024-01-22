aaa106cd9dd3f630d374d3295c4e78368fe4246e
Yiseul Ko <gls20020128@gmail.com>
2024-01-21 22:24:26 -0500

    29. Divide Two Integers (got help)

---
+++ 29. Divide Two Integers/Program.cs
@@ -0,0 +1,45 @@
+﻿Solution s = new Solution();
+int[] a = { 0, 1, 2, 1, 2 };
+Console.WriteLine(s.Divide(-2147483648, -1));
+
+public class Solution
+{
+    public int Divide(int dividend, int divisor)
+    {
+        // check sign
+        bool isPositive = (dividend > 0 && divisor > 0) || (dividend < 0 && divisor < 0) ? true : false;
+
+        // to long
+        long longDividend = (long)dividend;
+        long longDivisor = (long)divisor;
+        longDividend = Math.Abs(longDividend);
+        longDivisor = Math.Abs(longDivisor);
+
+        if (longDividend == longDivisor)
+        {
+            return isPositive ? 1 : -1;
+        }
+
+        // loop
+        long answer = 0;
+        while (longDividend >= longDivisor)
+        {
+            long multipliedBy = 1;
+            long sum = longDivisor;
+            while ((sum + sum) <= longDividend)
+            {
+                sum += sum;
+                multipliedBy += multipliedBy;
+            }
+            longDividend -= sum;
+            answer += multipliedBy;
+        }
+
+        if (answer > Int32.MaxValue)
+        {
+            return isPositive ? Int32.MaxValue : Int32.MinValue;
+        }
+
+        return isPositive ? Convert.ToInt32(answer) : -Convert.ToInt32(answer);
+    }
+}
