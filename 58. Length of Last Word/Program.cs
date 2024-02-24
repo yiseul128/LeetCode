746996c2fe34773cefe8e4b6e1746c239b89f6b1
Yiseul Ko <gls20020128@gmail.com>
2024-02-23 22:38:29 -0500

    58. Length of Last Word

---
+++ 58. Length of Last Word/Program.cs
@@ -0,0 +1,22 @@
+﻿public class Solution
+{
+    public int LengthOfLastWord(string s)
+    {
+        char space = ' ';
+
+        int end = s.Length - 1;
+        while (s[end] == space)
+        {
+            end--;
+        }
+
+        int count = 0;
+        while (end >= 0 && s[end] != space)
+        {
+            end--;
+            count++;
+        }
+
+        return count;
+    }
+}
