8bf500df0db65855045b3a6b61575822f41ce9c5
Yiseul Ko <gls20020128@gmail.com>
2024-02-28 22:41:40 -0500

    1137. N-th Tribonacci Number

---
+++ 1137. N-th Tribonacci Number/Program.cs
@@ -0,0 +1,26 @@
+﻿public class Solution
+{
+    public int Tribonacci(int n)
+    {
+        if (n == 0)
+        {
+            return 0;
+        }
+
+        if (n < 3)
+        {
+            return 1;
+        }
+
+        int[] memo = new int[n + 1];
+        memo[1] = 1;
+        memo[2] = 1;
+
+        for (int i = 3; i <= n; i++)
+        {
+            memo[i] = memo[i - 1] + memo[i - 2] + memo[i - 3];
+        }
+
+        return memo[n];
+    }
+}
