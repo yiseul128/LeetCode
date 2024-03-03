d9aea20fee804f2de8882ddbea86970703410dc2
Yiseul Ko <gls20020128@gmail.com>
2024-03-03 12:24:22 -0500

    392. Is Subsequence

---
+++ 392. Is Subsequence/Program.cs
@@ -0,0 +1,29 @@
+﻿public class Solution
+{
+    public bool IsSubsequence(string s, string t)
+    {
+        if (s.Length == 0)
+        {
+            return true;
+        }
+
+        int sIdx = 0;
+        int tIdx = 0;
+
+        while (tIdx < t.Length)
+        {
+            if (s[sIdx] == t[tIdx])
+            {
+                sIdx++;
+            }
+            tIdx++;
+
+            if (sIdx == s.Length)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
