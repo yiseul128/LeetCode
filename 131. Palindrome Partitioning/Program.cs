70e335f07a7d6b4937bb6351be339ff1795bfdca
Yiseul Ko <gls20020128@gmail.com>
2024-03-06 21:32:57 -0500

    131. Palindrome Partitioning

---
+++ 131. Palindrome Partitioning/Program.cs
@@ -0,0 +1,45 @@
+﻿public class Solution
+{
+    public IList<IList<string>> Partition(string s)
+    {
+        List<IList<string>> answer = new List<IList<string>>();
+        RecurPartition(s, answer, new List<string>(), 0, 1);
+        return answer;
+    }
+
+    public void RecurPartition(string s, IList<IList<string>> answer, IList<string> partition, int idx, int length)
+    {
+        if (idx + length > s.Length)
+        {
+            return;
+        }
+
+        string substr = s.Substring(idx, length);
+        int halfLen = length / 2;
+        bool isValid = true;
+        for (int i = 0; i < halfLen; i++)
+        {
+            if (substr[i] != substr[length - 1 - i])
+            {
+                isValid = false;
+                break;
+            }
+        }
+
+        // recur
+        RecurPartition(s, answer, new List<string>(partition), idx, length + 1);
+        if (isValid)
+        {
+            partition.Add(substr);
+
+            // add to answer
+            if (idx + length == s.Length)
+            {
+                answer.Add(partition);
+            }
+
+            // recur
+            RecurPartition(s, answer, new List<string>(partition), idx + length, 1);
+        }
+    }
+}
