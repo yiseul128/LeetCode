9f77e5267e58420a164d2cf821db0e78816978b7
Yiseul Ko <gls20020128@gmail.com>
2024-03-10 14:49:10 -0400

    2900. Longest Unequal Adjacent Groups Subsequence I

---
+++ 2900. Longest Unequal Adjacent Groups Subsequence I/Program.cs
@@ -0,0 +1,20 @@
+﻿public class Solution
+{
+    public IList<string> GetLongestSubsequence(string[] words, int[] groups)
+    {
+        IList<string> answer = new List<string>();
+
+        int prevBinary = -1;
+        for (int i = 0; i < words.Length; i++)
+        {
+            if (prevBinary != groups[i])
+            {
+                answer.Add(words[i]);
+                prevBinary = groups[i];
+            }
+        }
+
+        return answer;
+    }
+}
+}
