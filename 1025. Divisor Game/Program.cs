8c7066ce9d4738e7b15996e9a97ae28486a2afe4
Yiseul Ko <gls20020128@gmail.com>
2024-03-10 14:49:59 -0400

    1025. Divisor Game (got help)

---
+++ 1025. Divisor Game/Program.cs
@@ -0,0 +1,12 @@
+﻿public class Solution
+{
+    public bool DivisorGame(int n)
+    {
+        // n=1 => lose :(
+        // n=2 => x=1 => win :)
+
+        // if n%2==0 => can select n-1 as x
+        // if n%2!=0 => can only make choose odd num as => passing even num to opponent
+        return n % 2 == 0;
+    }
+}
