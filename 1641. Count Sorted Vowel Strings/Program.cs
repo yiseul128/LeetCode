9a7c44468fe4aac6937a9f5104e7906458ee2096
Yiseul Ko <gls20020128@gmail.com>
2024-03-14 00:59:10 -0400

    1641. Count Sorted Vowel Strings

---
+++ 1641. Count Sorted Vowel Strings/Program.cs
@@ -0,0 +1,25 @@
+﻿public class Solution
+{
+    public int CountVowelStrings(int n)
+    {
+        // currIdx => 1-5 representing each vowel
+        return RecurCount(n, 5);
+    }
+
+    public int RecurCount(int n, int currIdx)
+    {
+        // length met!
+        if (n == 0)
+        {
+            return 1;
+        }
+
+        int count = 0;
+        for (int i = currIdx; i > 0; i--)
+        {
+            count += RecurCount(n - 1, i);
+        }
+
+        return count;
+    }
+}
