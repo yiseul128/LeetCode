3d684523682d5006e026d0d20d21106ca2f5da74
Yiseul Ko <gls20020128@gmail.com>
2024-03-14 23:46:57 -0400

    647. Palindromic Substrings

---
+++ 647. Palindromic Substrings/Program.cs
@@ -0,0 +1,24 @@
+﻿public class Solution
+{
+    public int CountSubstrings(string s)
+    {
+        int answer = 0;
+        int n = s.Length;
+        for (int mid = 0; mid < n; mid++)
+        {
+            // odd len
+            for (int start = mid, end = mid; start >= 0 && end < n && s[start] == s[end]; start--, end++)
+            {
+                answer++;
+            }
+
+            // even len
+            for (int start = mid - 1, end = mid; start >= 0 && end < n && s[start] == s[end]; start--, end++)
+            {
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+}
