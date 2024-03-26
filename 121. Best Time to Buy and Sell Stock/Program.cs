472997acac8f9ccf2563643c289d5c5a99ab55df
Yiseul Ko <gls20020128@gmail.com>
2024-03-25 21:09:33 -0400

    Alternative solution for 121. Best Time to Buy and Sell Stock

--- 121. Best Time to Buy and Sell Stock/Program.cs
+++ 121. Best Time to Buy and Sell Stock/Program.cs
@@ -15,20 +15,43 @@ namespace _121.Best_Time_to_Buy_and_Sell_Stock
             Console.WriteLine(s.MaxProfit(prices)); //5
         }
     }
+
+    /* sol 1 */
+    //public class Solution
+    //{
+    //    public int MaxProfit(int[] prices)
+    //    { 
+    //        int max = 0, min = 0;
+
+    //        for (int i = 1; i < prices.Length; i++)
+    //        {
+    //            max = max > prices[i] - prices[min] ? max : prices[i] - prices[min];
+    //            min = prices[min] < prices[i] ? min : i;
+    //        }
+
+    //        return max;
+
+    //    }
+    //}
+
+    /* sol 2 */
     public class Solution
     {
         public int MaxProfit(int[] prices)
-        { 
-            int max = 0, min = 0;
+        {
+            int[] maxMemo = new int[prices.Length];
 
-            for (int i = 1; i < prices.Length; i++)
+            maxMemo[prices.Length - 1] = prices[prices.Length - 1];
+
+            int maxProfit = 0;
+            for (int idx = prices.Length - 2; idx >= 0; idx--)
             {
-                max = max > prices[i] - prices[min] ? max : prices[i] - prices[min];
-                min = prices[min] < prices[i] ? min : i;
+                maxMemo[idx] = maxMemo[idx + 1] > prices[idx] ? maxMemo[idx + 1] : prices[idx];
+                int newMaxProfit = maxMemo[idx] - prices[idx];
+                maxProfit = newMaxProfit > maxProfit ? newMaxProfit : maxProfit;
             }
 
-            return max;
-
+            return maxProfit;
         }
     }
 }
