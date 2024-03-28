0ee464214017f3de7633ad021b60a05697bd9f09
Yiseul Ko <gls20020128@gmail.com>
2024-03-28 19:50:54 -0400

    2110. Number of Smooth Descent Periods of a Stock

---
+++ 2110. Number of Smooth Descent Periods of a Stock/Program.cs
@@ -0,0 +1,21 @@
+﻿public class Solution
+{
+    public long GetDescentPeriods(int[] prices)
+    {
+        int smoothCount = 1;
+        long answer = 1;
+        for (int i = 1; i < prices.Length; i++)
+        {
+            if (prices[i - 1] - prices[i] == 1)
+            {
+                smoothCount++;
+            }
+            else
+            {
+                smoothCount = 1;
+            }
+            answer += smoothCount;
+        }
+        return answer;
+    }
+}
