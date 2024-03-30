5044e7a750477359ea0b63aec8272b3da1a9341f
Yiseul Ko <gls20020128@gmail.com>
2024-03-30 09:40:15 -0400

    714. Best Time to Buy and Sell Stock with Transaction Fee

---
+++ 714. Best Time to Buy and Sell Stock with Transaction Fee/Program.cs
@@ -0,0 +1,25 @@
+﻿public class Solution
+{
+    public int MaxProfit(int[] prices, int fee)
+    {
+        // buy => sell
+        // buy => rest (might repeat resting)
+        int[] bought = new int[prices.Length];
+        bought[0] = -prices[0];
+
+        // sell => buy
+        // sell => rest (might repeat resting)
+        int[] sold = new int[prices.Length];
+
+        for (int i = 1; i < prices.Length; i++)
+        {
+            // rest || buy
+            bought[i] = Math.Max(bought[i - 1], sold[i - 1] - prices[i]);
+
+            // rest || sell 
+            sold[i] = Math.Max(sold[i - 1], bought[i - 1] + prices[i] - fee);
+        }
+
+        return sold[prices.Length - 1];
+    }
+}
