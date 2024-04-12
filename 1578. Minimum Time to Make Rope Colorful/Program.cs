bacdad87ab999fe0f917c6a8132dd4599c0c6092
Yiseul Ko <gls20020128@gmail.com>
2024-04-11 21:31:53 -0400

    1578. Minimum Time to Make Rope Colorful

---
+++ 1578. Minimum Time to Make Rope Colorful/Program.cs
@@ -0,0 +1,46 @@
+﻿public class Solution
+{
+    public int MinCost(string colors, int[] neededTime)
+    {
+        int cost = 0;
+
+        int idx = 1;
+        int maxTimeIdx = -1;
+   
+        int timeUsed = neededTime[0];
+        while (idx < colors.Length)
+        {
+            if (colors[idx] == colors[idx - 1])
+            {
+                if (maxTimeIdx == -1)
+                {
+                    maxTimeIdx = neededTime[idx] > neededTime[idx - 1] ? idx : idx - 1;
+                }
+                else
+                {
+                    maxTimeIdx = neededTime[idx] > neededTime[maxTimeIdx] ? idx : maxTimeIdx;
+                }
+                timeUsed += neededTime[idx];
+            }
+            else
+            {
+                if (maxTimeIdx != -1)
+                {
+                    cost += timeUsed - neededTime[maxTimeIdx];
+                }
+
+                timeUsed = neededTime[idx];
+                maxTimeIdx = -1;
+            }
+            idx++;
+        }
+
+        if (maxTimeIdx != -1)
+        {
+            cost += timeUsed - neededTime[maxTimeIdx];
+        }
+
+        return cost;
+    }
+
+}
