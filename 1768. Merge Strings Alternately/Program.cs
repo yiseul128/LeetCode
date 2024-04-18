30be333296af79a0b9ec07cd6720c224fe98afc3
Yiseul Ko <gls20020128@gmail.com>
2024-04-17 21:00:12 -0400

    1768. Merge Strings Alternately

---
+++ 1768. Merge Strings Alternately/Program.cs
@@ -0,0 +1,24 @@
+﻿public class Solution
+{
+    public string MergeAlternately(string word1, string word2)
+    {
+        char[] merged = new char[word1.Length + word2.Length];
+
+        int n = word1.Length > word2.Length ? word1.Length : word2.Length;
+        int idx = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (word1.Length > i)
+            {
+                merged[idx++] = word1[i];
+            }
+
+            if (word2.Length > i)
+            {
+                merged[idx++] = word2[i];
+            }
+        }
+
+        return new string(merged);
+    }
+}
