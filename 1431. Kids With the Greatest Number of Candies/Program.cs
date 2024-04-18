58c1c3f88e458ac402564573518a9975f0995d67
Yiseul Ko <gls20020128@gmail.com>
2024-04-17 21:01:32 -0400

    1431. Kids With the Greatest Number of Candies

---
+++ 1431. Kids With the Greatest Number of Candies/Program.cs
@@ -0,0 +1,29 @@
+﻿public class Solution
+{
+    public IList<bool> KidsWithCandies(int[] candies, int extraCandies)
+    {
+        IList<bool> isGreatest = new List<bool>();
+
+        // find existing greatest
+        int greatestIdx = 0;
+        for (int i = 1; i < candies.Length; i++)
+        {
+            greatestIdx = candies[greatestIdx] > candies[i] ? greatestIdx : i;
+        }
+
+        // find out by comparing existing greatest and ith + extra
+        for (int i = 0; i < candies.Length; i++)
+        {
+            if (candies[greatestIdx] <= candies[i] + extraCandies)
+            {
+                isGreatest.Add(true);
+            }
+            else
+            {
+                isGreatest.Add(false);
+            }
+        }
+
+        return isGreatest;
+    }
+}
