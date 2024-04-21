d2155d4c81be27938f8bb6ba69ed6f96e01ecfdf
Yiseul Ko <gls20020128@gmail.com>
2024-04-21 00:57:28 -0400

    151. Reverse Words in a String

---
+++ 151. Reverse Words in a String/Program.cs
@@ -0,0 +1,20 @@
+﻿public class Solution
+{
+    public string ReverseWords(string s)
+    {
+        string[] words = s.Split(' ');
+
+        string answer = "";
+        for (int i = words.Length - 1; i >= 0; i--)
+        {
+            words[i] = words[i].Replace(" ", "").Replace("\t", "");
+
+            if (words[i].Length > 0)
+            {
+                answer += " " + words[i];
+            }
+        }
+
+        return answer.Substring(1);
+    }
+}
