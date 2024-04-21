2ad9b94bd6b08ed16ddea097699212f76f759b48
Yiseul Ko <gls20020128@gmail.com>
2024-04-21 17:29:59 -0400

    443. String Compression

---
+++ 443. String Compression/Program.cs
@@ -0,0 +1,46 @@
+﻿public class Solution
+{
+    public int Compress(char[] chars)
+    {
+        int count = 1;
+        char currChar = chars[0];
+        int idx = 0;
+
+        for (int i = 1; i < chars.Length; i++)
+        {
+            if (chars[i - 1] == chars[i])
+            {
+                count++;
+            }
+            else
+            {
+                chars[idx++] = currChar;
+
+                if (count != 1)
+                {
+                    string strCount = count.ToString();
+                    for (int j = 0; j < strCount.Length; j++)
+                    {
+                        chars[idx++] = strCount[j];
+                    }
+                }
+
+                count = 1;
+                currChar = chars[i];
+            }
+        }
+
+        chars[idx++] = currChar;
+
+        if (count != 1)
+        {
+            string strCount = count.ToString();
+            for (int j = 0; j < strCount.Length; j++)
+            {
+                chars[idx++] = strCount[j];
+            }
+        }
+
+        return idx;
+    }
+}
