66b258c2707b571f105b740aebebc5835585f3df
Yiseul Ko <gls20020128@gmail.com>
2024-04-26 00:30:03 -0400

    1679. Max Number of K-Sum Pairs

---
+++ 1679. Max Number of K-Sum Pairs/Program.cs
@@ -0,0 +1,34 @@
+﻿public class Solution
+{
+    public int MaxOperations(int[] nums, int k)
+    {
+        // sort
+        Array.Sort(nums);
+
+        int count = 0;
+
+        // iterate
+        int left = 0;
+        int right = nums.Length - 1;
+        while (left < right)
+        {
+            if (nums[left] + nums[right] == k)
+            {
+                // match
+                count++;
+                left++;
+                right--;
+            }
+            else if (nums[left] + nums[right] < k)
+            {
+                left++;
+            }
+            else
+            {
+                right--;
+            }
+        }
+
+        return count;
+    }
+}
