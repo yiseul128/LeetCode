df1580678a3d780d365473372105d2e372892a42
Yiseul Ko <gls20020128@gmail.com>
2024-04-26 19:51:18 -0400

    643. Maximum Average Subarray I

---
+++ 643. Maximum Average Subarray I/Program.cs
@@ -0,0 +1,22 @@
+﻿public class Solution
+{
+    public double FindMaxAverage(int[] nums, int k)
+    {
+        double sum = 0;
+        for (int i = 0; i < k; i++)
+        {
+            sum += nums[i];
+        }
+
+        double maxSum = sum;
+        int end = k;
+        while (end < nums.Length)
+        {
+            sum -= nums[end - k];
+            sum += nums[end];
+            end++;
+            maxSum = maxSum > sum ? maxSum : sum;
+        }
+        return maxSum / k;
+    }
+}
