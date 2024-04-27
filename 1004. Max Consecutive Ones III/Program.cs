a442f6d3b51d9fc6d1a8b218ba2852bc85e31220
Yiseul Ko <gls20020128@gmail.com>
2024-04-27 13:13:23 -0400

    1004. Max Consecutive Ones III

---
+++ 1004. Max Consecutive Ones III/Program.cs
@@ -0,0 +1,37 @@
+﻿public class Solution
+{
+    public int LongestOnes(int[] nums, int k)
+    {
+        int max = 0;
+
+        int start = 0;
+        int end = 0;
+        // find first end with k flipped
+        while (0 != k && end < nums.Length)
+        {
+            // flip
+            if (nums[end++] == 0)
+            {
+                k--;
+            }
+        }
+        max = max > end - start ? max : end - start;
+
+        // slide until end of nums
+        while (end < nums.Length)
+        {
+            // flip
+            if (nums[end++] == 0)
+            {
+                while (nums[start] != 0)
+                {
+                    start++;
+                }
+                start++;
+            }
+            max = max > end - start ? max : end - start;
+        }
+
+        return max;
+    }
+}
