e40f7d1c23e4d34a4623e6ec79a9a7ce24935a79
Yiseul Ko <gls20020128@gmail.com>
2024-04-27 13:12:41 -0400

    1456. Maximum Number of Vowels in a Substring of Given Length

---
+++ 1456. Maximum Number of Vowels in a Substring of Given Length/Program.cs
@@ -0,0 +1,36 @@
+﻿public class Solution
+{
+    public int MaxVowels(string s, int k)
+    {
+        List<char> vowels = new List<char> { 'a', 'e', 'i', 'o', 'u' };
+
+        int count = 0;
+        for (int i = 0; i < k; i++)
+        {
+            if (vowels.Contains(s[i]))
+            {
+                count++;
+            }
+        }
+
+        int max = count;
+        int end = k;
+        while (end < s.Length)
+        {
+            if (vowels.Contains(s[end - k]))
+            {
+                count--;
+            }
+
+            if (vowels.Contains(s[end]))
+            {
+                count++;
+            }
+
+            end++;
+            max = max > count ? max : count;
+        }
+
+        return max;
+    }
+}
