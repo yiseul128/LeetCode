9c54786857701686f627998f391135fc7baff480
Yiseul Ko <gls20020128@gmail.com>
2024-04-28 16:20:19 -0400

    1207. Unique Number of Occurrences

---
+++ 1207. Unique Number of Occurrences/Program.cs
@@ -0,0 +1,31 @@
+﻿public class Solution
+{
+    public bool UniqueOccurrences(int[] arr)
+    {
+        Dictionary<int, int> nums = new Dictionary<int, int>();
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (nums.ContainsKey(arr[i]))
+            {
+                nums[arr[i]]++;
+            }
+            else
+            {
+                nums.Add(arr[i], 1);
+            }
+        }
+
+        HashSet<int> uniqueCounts = new HashSet<int>();
+        foreach (var item in nums)
+        {
+            if (uniqueCounts.Contains(item.Value))
+            {
+                return false;
+            }
+
+            uniqueCounts.Add(item.Value);
+        }
+
+        return true;
+    }
+}
