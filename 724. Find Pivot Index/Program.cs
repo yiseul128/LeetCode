8248fdd5f9da858f630ef0bba570162f9b1e6f6b
Yiseul Ko <gls20020128@gmail.com>
2024-04-28 15:52:19 -0400

    724. Find Pivot Index

---
+++ 724. Find Pivot Index/Program.cs
@@ -0,0 +1,26 @@
+﻿public class Solution
+{
+    public int PivotIndex(int[] nums)
+    {
+        int rightSum = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            rightSum += nums[i];
+        }
+
+        int leftSum = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            rightSum -= nums[i];
+
+            if (rightSum == leftSum)
+            {
+                return i;
+            }
+
+            leftSum += nums[i];
+        }
+
+        return -1;
+    }
+}
