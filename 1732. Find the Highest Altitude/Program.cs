38a2165d275cf01d71e947b4adfa61ce5de81217
Yiseul Ko <gls20020128@gmail.com>
2024-04-28 15:53:37 -0400

    1732. Find the Highest Altitude

---
+++ 1732. Find the Highest Altitude/Program.cs
@@ -0,0 +1,16 @@
+﻿public class Solution
+{
+    public int LargestAltitude(int[] gain)
+    {
+        int max = 0;
+        int altitude = 0;
+
+        for (int i = 0; i < gain.Length; i++)
+        {
+            altitude += gain[i];
+            max = max > altitude ? max : altitude;
+        }
+
+        return max;
+    }
+}
