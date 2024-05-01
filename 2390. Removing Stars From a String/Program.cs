c6cdf8d9e9c174bccc698ee6d701886130fede8b
Yiseul Ko <gls20020128@gmail.com>
2024-04-30 21:40:55 -0400

    2390. Removing Stars From a String

---
+++ 2390. Removing Stars From a String/Program.cs
@@ -0,0 +1,26 @@
+﻿public class Solution
+{
+    public string RemoveStars(string s)
+    {
+        Stack<char> processedStack = new Stack<char>();
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (s[i] == '*')
+            {
+                processedStack.Pop();
+            }
+            else
+            {
+                processedStack.Push(s[i]);
+            }
+        }
+
+        char[] charArr = new char[processedStack.Count];
+        int idx = charArr.Length - 1;
+        while (processedStack.Count > 0)
+        {
+            charArr[idx--] = processedStack.Pop();
+        }
+        return new string(charArr);
+    }
+}
