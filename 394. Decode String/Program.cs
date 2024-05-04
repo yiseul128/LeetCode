19025914e7f558c34ee4cc6e06a5c6abdc27476d
Yiseul Ko <gls20020128@gmail.com>
2024-05-03 23:12:03 -0400

    Improve runtime of 394. Decode String (got help)

--- 394. Decode String/Program.cs
+++ 394. Decode String/Program.cs
@@ -14,47 +14,92 @@ namespace _394.Decode_String
     }
     public class Solution
     {
-        string num = "0123456789";
-        int end = -1;
-
         public string DecodeString(string s)
         {
-            return Decode(s, 0);
-        }
+            Stack<int> intStack = new Stack<int>();
+            Stack<string> strStack = new Stack<string>();
 
-        public string Decode(string str, int start)
-        {
-            string decoded = "";
+            string curr = "";
+            int k = 0;
 
-            int numStart = -1;
-            while (start < str.Length)
+            for (int i = 0; i < s.Length; i++)
             {
-                if (str[start] == '[')
+                if (char.IsNumber(s[i]))
                 {
-                    decoded += String.Concat(Enumerable.Repeat(Decode(str, start + 1), Convert.ToInt32(str.Substring(numStart, start - numStart))));
-                    start = end;
-                    numStart = -1;
+                    k = k * 10 + int.Parse(s[i].ToString());
                 }
-                else if (str[start] == ']')
+                else if (s[i] == '[')
                 {
-                    end = start;
-                    return decoded;
+                    // push to stack
+                    intStack.Push(k);
+                    strStack.Push(curr);
+
+                    // reset
+                    curr = "";
+                    k = 0;
                 }
-                else if (num.Contains(str[start]))
+                else if (s[i] == ']')
                 {
-                    if (numStart == -1)
+                    // generate 
+                    string newStr = "";
+                    for (k = intStack.Pop(); k > 0; k--)
                     {
-                        numStart = start;
+                        newStr += curr;
                     }
+                    // curr = prev + new
+                    curr = strStack.Pop() + newStr;
                 }
                 else
                 {
-                    decoded += str[start].ToString();
+                    curr += s[i].ToString();
                 }
-                start++;
             }
 
-            return decoded;
+            return curr;
         }
+
+        /* recursive sln */
+        //string num = "0123456789";
+        //int end = -1;
+
+        //public string DecodeString(string s)
+        //{
+        //    return Decode(s, 0);
+        //}
+
+        //public string Decode(string str, int start)
+        //{
+        //    string decoded = "";
+
+        //    int numStart = -1;
+        //    while (start < str.Length)
+        //    {
+        //        if (str[start] == '[')
+        //        {
+        //            decoded += String.Concat(Enumerable.Repeat(Decode(str, start + 1), Convert.ToInt32(str.Substring(numStart, start - numStart))));
+        //            start = end;
+        //            numStart = -1;
+        //        }
+        //        else if (str[start] == ']')
+        //        {
+        //            end = start;
+        //            return decoded;
+        //        }
+        //        else if (num.Contains(str[start]))
+        //        {
+        //            if (numStart == -1)
+        //            {
+        //                numStart = start;
+        //            }
+        //        }
+        //        else
+        //        {
+        //            decoded += str[start].ToString();
+        //        }
+        //        start++;
+        //    }
+
+        //    return decoded;
+        //}
     }
 }
