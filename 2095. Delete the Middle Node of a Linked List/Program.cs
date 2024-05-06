f65655904e2681b46d21168c94247f709539e385
Yiseul Ko <gls20020128@gmail.com>
2024-05-06 19:59:50 -0400

    2095. Delete the Middle Node of a Linked List

---
+++ 2095. Delete the Middle Node of a Linked List/Program.cs
@@ -0,0 +1,39 @@
+﻿/* Definition for singly-linked list. */
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
+ 
+public class Solution
+{
+    public ListNode DeleteMiddle(ListNode head)
+    {
+        if (head.next == null)
+        {
+            return null;
+        }
+
+        ListNode prevMiddle = head;
+        ListNode middle = head.next;
+        ListNode walk = head.next;
+        bool moveMiddle = false;
+        while (walk.next != null)
+        {
+            if (moveMiddle)
+            {
+                prevMiddle = middle;
+                middle = middle.next;
+            }
+            walk = walk.next;
+            moveMiddle = !moveMiddle;
+        }
+        prevMiddle.next = middle.next;
+        return head;
+    }
+}
