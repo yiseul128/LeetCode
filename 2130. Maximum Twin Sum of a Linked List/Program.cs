3effba6b0f079aa1b352a9d4a2b0bf815be5f5d2
Yiseul Ko <gls20020128@gmail.com>
2024-05-06 20:00:53 -0400

    2130. Maximum Twin Sum of a Linked List

---
+++ 2130. Maximum Twin Sum of a Linked List/Program.cs
@@ -0,0 +1,43 @@
+﻿/* Definition for singly-linked list. */
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class Solution
+{
+    public int PairSum(ListNode head)
+    {
+        ListNode walk = head;
+        int count = 0;
+        while (walk != null)
+        {
+            walk = walk.next;
+            count++;
+        }
+
+        int half = count / 2;
+        int[] firstHalfArr = new int[half];
+        int max = Int32.MinValue;
+        for (int i = 0; i < count; i++)
+        {
+            if (half > i)
+            {
+                firstHalfArr[i] = head.val;
+            }
+            else
+            {
+                int sum = head.val + firstHalfArr[count - 1 - i];
+                max = sum > max ? sum : max;
+            }
+            head = head.next;
+        }
+        return max;
+    }
+}
