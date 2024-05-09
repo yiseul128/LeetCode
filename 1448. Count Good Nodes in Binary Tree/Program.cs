4c3aecb14c04f8a4f19a76bd1b35ea53208b8e89
Yiseul Ko <gls20020128@gmail.com>
2024-05-08 21:57:46 -0400

    1448. Count Good Nodes in Binary Tree

---
+++ 1448. Count Good Nodes in Binary Tree/Program.cs
@@ -0,0 +1,42 @@
+﻿/* Definition for a binary tree node.  */
+public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class Solution
+{
+    public int GoodNodes(TreeNode root)
+    {
+        return RecurGoodNodes(root, Int32.MinValue);
+    }
+
+    public int RecurGoodNodes(TreeNode currNode, int max)
+    {
+        int count = 0;
+        if (currNode.val >= max)
+        {
+            count++;
+            max = currNode.val;
+        }
+
+        if (currNode.left != null)
+        {
+            count += RecurGoodNodes(currNode.left, max);
+        }
+        if (currNode.right != null)
+        {
+            count += RecurGoodNodes(currNode.right, max);
+        }
+
+        return count;
+    }
+}
